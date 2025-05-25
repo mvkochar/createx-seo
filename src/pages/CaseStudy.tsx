bc188f4ebb591edf1ac32ffa0e351d7fcfc07c4b
sgkochar <sgkochar@gmail.com>
2025-05-25 13:54:16 +0300

    edit CaseSudy page

--- src/pages/CaseStudy.tsx
+++ src/pages/CaseStudy.tsx
@@ -1,5 +1,6 @@
 import { BarChart } from '@mui/x-charts/BarChart'
 import './css/CaseStudy.css'
+import { SeoAnalysis, Studies, Testimonials } from '../components'
 
 const CaseStudy = () => {
     return (
@@ -129,21 +130,78 @@ const CaseStudy = () => {
                     </p>
                     <div className='study-pic'><img src="/images/study-pic.png" alt="study-pic" /></div>
                     <h2 className="study-bl-title">Resieved goals</h2>
-                    <ul className="study-bl-list" style={{marginBottom: "60px"}}>
+                    <ul className="study-bl-list" style={{ marginBottom: "60px" }}>
                         <li>Bibendum malesuada mauris, id nisl enim diam.</li>
                         <li>Nam id in non sed cras purus nunc et.</li>
                         <li>Neque tortor, pellentesque amet est, eu accumsan condimentum nulla.</li>
                         <li>Mauris orci, cursus nisl odio est adipiscing gravida magna eget.</li>
                         <li>Sit turpis sit ultrices viverra adipiscing malesuada.</li>
                     </ul>
-                    <h2 className="study-bl-title">Results</h2>        
+                    <h2 className="study-bl-title">Results</h2>
                     <ul className="study-bl-list">
-                         <li>Mauris dignissim blandit cursus imperdiet accumsan lorem vel, tortor.</li>
-                         <li>Quis mauris vel felis convallis nulla dignissim.</li>
-                         <li>Cursus sit platea ac posuere non interdum in.</li>
-                         <li>Mattis pellentesque vel, venenatis, urna, quis ut. </li>   
-                    </ul>    
+                        <li>Mauris dignissim blandit cursus imperdiet accumsan lorem vel, tortor.</li>
+                        <li>Quis mauris vel felis convallis nulla dignissim.</li>
+                        <li>Cursus sit platea ac posuere non interdum in.</li>
+                        <li>Mattis pellentesque vel, venenatis, urna, quis ut. </li>
+                    </ul>
                 </div>
+                <div>
+                    <div className="study-about d-f">
+                        <div>
+                            <h4 className="about-bl-title">About the client</h4>
+                            <p className="about-bl-desc">
+                                At facilisi sapien posuere eget nunc senectus proin nullam.
+                                Tortor senectus in et sagittis, vitae diam cras dignissim.
+                                Varius adipiscing eget diam nisi. Orci, consectetur vulputate
+                                metus ornare pharetra, neque, fermentum.
+                            </p>
+                        </div>
+                        <div>
+                            <h4 className="about-bl-title">Industry</h4>
+                            <p className="about-bl-desc">Electric company - B2B</p>
+                        </div>
+                        <div>
+                            <h4 className="about-bl-title">Services rendered</h4>
+                            <ul className="about-bl-list">
+                                <li>Remarketing</li>
+                                <li>Conversion Rate Optimization</li>
+                                <li>User Testing</li>
+                                <li>PPC Management</li>
+                                <li>Usability Testing</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="study-tags d-f">
+                        <div className="tag text-uppercase">#seo</div>
+                        <div className="tag">#research</div>
+                        <div className="tag">#payed_traffic</div>
+                    </div>
+                    <div className="study-share d-f">
+                        <h5 className="study-share-title">Share:</h5>
+                        <a href="" className="d-b">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M14.6354 0.836822L12.321 0.833008C9.72086 0.833008 8.04054 2.60392 8.04054 5.34487V7.42513H5.71352C5.51244 7.42513 5.34961 7.59259 5.34961 7.79915V10.8132C5.34961 11.0198 5.51263 11.187 5.71352 11.187H8.04054V18.7925C8.04054 18.9991 8.20337 19.1663 8.40445 19.1663H11.4405C11.6416 19.1663 11.8045 18.9989 11.8045 18.7925V11.187H14.5253C14.7264 11.187 14.8892 11.0198 14.8892 10.8132L14.8903 7.79915C14.8903 7.69997 14.8519 7.60499 14.7837 7.5348C14.7156 7.46461 14.6227 7.42513 14.5262 7.42513H11.8045V5.66166C11.8045 4.81407 12.0011 4.38379 13.0759 4.38379L14.635 4.38322C14.8359 4.38322 14.9987 4.21576 14.9987 4.00939V1.21065C14.9987 1.00447 14.8361 0.837204 14.6354 0.836822Z" fill="#787A80" />
+                            </svg>
+                        </a>
+                        <a href="" className="d-b">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M18.75 4.8106C18.1059 5.08388 17.4148 5.26922 16.6887 5.35196C17.4301 4.92683 17.9976 4.25252 18.2666 3.45149C17.5711 3.84519 16.8035 4.13106 15.9855 4.28603C15.3305 3.61695 14.3988 3.2002 13.3653 3.2002C11.3827 3.2002 9.77523 4.73942 9.77523 6.63674C9.77523 6.90583 9.80694 7.16865 9.86819 7.41997C6.88501 7.2765 4.23973 5.90797 2.46928 3.82843C2.1598 4.33523 1.98374 4.92576 1.98374 5.55613C1.98374 6.74878 2.618 7.8011 3.58033 8.41677C2.992 8.39793 2.43866 8.24296 1.95423 7.98537V8.0283C1.95423 9.69317 3.19213 11.0826 4.83353 11.3989C4.5328 11.4764 4.21568 11.5193 3.88761 11.5193C3.6558 11.5193 3.43161 11.4973 3.2118 11.4554C3.66889 12.8219 4.99429 13.8156 6.56463 13.8428C5.33657 14.7642 3.7881 15.3119 2.10624 15.3119C1.81646 15.3119 1.53103 15.2951 1.25 15.2647C2.83893 16.2417 4.7253 16.8113 6.75273 16.8113C13.3566 16.8113 16.9665 11.5738 16.9665 7.0315L16.9544 6.5865C17.6597 6.1048 18.2699 5.49959 18.75 4.8106Z" fill="#787A80" />
+                            </svg>
+                        </a>
+                        <a href="" className="d-b">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M18.3285 18.3337L18.3327 18.333V12.2205C18.3327 9.23019 17.6889 6.92671 14.1931 6.92671C12.5125 6.92671 11.3848 7.84894 10.9243 8.72324H10.8757V7.20588H7.56115V18.333H11.0125V12.8232C11.0125 11.3726 11.2875 9.96977 13.0841 9.96977C14.8542 9.96977 14.8806 11.6253 14.8806 12.9163V18.3337H18.3285Z" fill="#787A80" />
+                                <path d="M1.94101 7.20657H5.39657V18.3337H1.94101V7.20657Z" fill="#787A80" />
+                                <path d="M3.6674 1.66699C2.56254 1.66699 1.66602 2.56352 1.66602 3.66838C1.66602 4.77324 2.56254 5.68852 3.6674 5.68852C4.77227 5.68852 5.66879 4.77324 5.66879 3.66838C5.6681 2.56352 4.77157 1.66699 3.6674 1.66699Z" fill="#787A80" />
+                            </svg>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <SeoAnalysis/>
+            <div className="study-bottom">
+                <Testimonials/>
+                <Studies/>
             </div>
         </>
     )
